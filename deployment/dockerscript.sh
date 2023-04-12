1a0950d2e8cf14fb96c8cf7fe650e37dd8317adb
John <xxsuperduperxx0@gmail.com>
2023-04-12 18:17:20 +0000

    fixing script and other things

--- deployment/dockerscript.sh
+++ deployment/dockerscript.sh
@@ -1,5 +1,7 @@
 #!bin/bash
+sudo docker stop httpd-docker-01
+sudo docker rm httpd-docker-01
 #Pull the latest docker image from dockerhub
 sudo docker pull imatsoup/ceg3120-john:latest
 #Then, restart the docker container
-sudo docker restart httpd-docker-01
+sudo docker run -d --name httpd-docker-01 -p 80:80 imatsoup/ceg3120-john
